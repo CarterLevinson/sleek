4bd533d03cb3cfce0c87be028dc5fd1bba1a5813
Carter S. Levinson <carter@levinson.cc>
2023-09-20 17:05:25 -0500

    Update search design

--- static/main.js
+++ static/main.js
@@ -1,14 +1,14 @@
 function goToSearchPage(query) {
-  window.location.href = "/search?query=" + query;
+  location.href = "/search?query=" + query;
 }
 
-function searchOnEnterKey(event, query) {
+function goToSearchOnEnter(event, query) {
   if(event.key === "Enter" || event.keyCode === 13) {
     goToSearchPage(query);
   }
 }
 
-function searchOnClick(event, query) {
+function goToSearchOnClick(event, query) {
   event.preventDefault();
   goToSearchPage(query);
 }
@@ -17,13 +17,26 @@ function init() {
   var $navInput = document.getElementById("navbar-search-input");
   if ($navInput !== null) {
     $navInput.addEventListener("keydown", function(event) {
-      searchOnEnterKey(event, $navInput.value.trim());
+      goToSearchOnEnter(event, $navInput.value.trim());
     });
 
     var $navButton = document.getElementById("navbar-search-button");
     $navButton.addEventListener("click", function(event) {
-      searchOnClick(event, $navInput.value.trim());
+      goToSearchOnClick(event, $navInput.value.trim());
     });
+  }
+
+  var $searchInput = document.getElementById("search-input");
+  if (window.location.pathname === "/" && $searchInput !== null) {
+    $searchInput.addEventListener("keydown", function(event) {
+      goToSearchOnEnter(event, $searchInput.value.trim());
+    });
+
+    var $searchButton = document.getElementById("search-button");
+    $searchButton.addEventListener("click", function(event) {
+      goToSearchOnClick(event, $searchInput.value.trim());
+    });
+
   }
 }
 
